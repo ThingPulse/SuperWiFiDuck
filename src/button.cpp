fd23f73046d372859d3129202f70b5e327c0c230
Daniel Eichhorn <dani.eichhorn@squix.ch>
2026-07-18 07:25:31 +0200

    Adjust sensitivity

--- src/button.cpp
+++ src/button.cpp
@@ -3,32 +3,52 @@
 
 #if defined(TOUCH_ENABLED)
 
+#ifndef TOUCH_DEBOUNCE_MS
+#define TOUCH_DEBOUNCE_MS 50
+#endif
+
 namespace button {
-  
-    boolean isButtonPressed = false;
+
+    volatile boolean touchEventPending = false;
     boolean isTouched = false;
+    boolean lastTouchStatus = false;
+    unsigned long statusChangedAt = 0;
 
     void begin() {
         touchAttachInterrupt(TOUCH_PIN, touchCallback, TOUCH_THRESHOLD);
     }
 
     void touchCallback() {
-        isButtonPressed = true;
+        touchEventPending = true;
     }
 
 
-    bool isPressed() { 
-        if (isButtonPressed) {
-            bool lastStatus = touchInterruptGetLastStatus(TOUCH_PIN);
-            if (!isTouched && lastStatus) {
+    bool isPressed() {
+        if (touchEventPending) {
+            const bool currentStatus = touchInterruptGetLastStatus(TOUCH_PIN);
+            const unsigned long now = millis();
+
+            if (currentStatus != lastTouchStatus) {
+                lastTouchStatus = currentStatus;
+                statusChangedAt = now;
+            }
+
+            if ((now - statusChangedAt) < TOUCH_DEBOUNCE_MS) {
+                return false;
+            }
+
+            if (!isTouched && currentStatus) {
                 Serial.println(" --- T1 Touched");
                 isTouched = true;
                 return false;
-            } else if (isTouched && !lastStatus) {
+            } else if (isTouched && !currentStatus) {
                 Serial.println(" --- T1 Released");
-                isButtonPressed = false;
+                touchEventPending = false;
                 isTouched = false;
                 return true;
+            } else if (!isTouched && !currentStatus) {
+                // Ignore a noise pulse that did not remain active long enough.
+                touchEventPending = false;
             }
         }
         return false;
